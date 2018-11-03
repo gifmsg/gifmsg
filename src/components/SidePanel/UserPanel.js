295c0f94c65a2a1387d381f98db7910996497066
Paul Suarez <paulsblogging.com@gmail.com>
2018-11-03 15:22:50 -0700

    users can add channels and they are saved with meta data to the firebase database

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import firebase from '../../firebase';
-
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 
 
 class UserPanel extends React.Component {
+
+    state = {
+        user: this.props.currentUser
+    }
+
+
+
+
     dropdownOptions = () => [
         {
             key: 'user',
-            text: <span>Signed in as <strong>User</strong></span>,
+            text: (
+                <span>
+                    Signed in as <strong>{this.state.user.displayName}</strong>
+                </span>
+            ),
             disabled: true
         },
         {
@@ -31,6 +43,9 @@ class UserPanel extends React.Component {
     }
 
     render() {
+
+        const { user } = this.state;
+        
         return (
             <Grid style={{ background: '#4c3c4c' }}>
                 <Grid.Column>
@@ -39,13 +54,18 @@ class UserPanel extends React.Component {
                             <Icon name="talk" />
                             <Header.Content>GIFmsg</Header.Content>
                         </Header>
+                    <Header style={{ padding: '0.25em' }} as='h4' inverted>
+                        <Dropdown
+                            trigger={
+                                <span>
+                                    <Image src={user.photoURL} spaced="right" avatar />
+                                    {user.displayName}
+                                </span>}
+                            options={this.dropdownOptions()}
+                        />
+                    </Header>
                     </Grid.Row>
 
-                    <Header style={{ padding: '0.25em' }} as='h4' inverted>
-                        <Dropdown trigger={
-                            <span>User</span>
-                        } options={this.dropdownOptions()} />
-                    </Header>
                 </Grid.Column>
             
             </Grid>
@@ -53,5 +73,7 @@ class UserPanel extends React.Component {
         );
     }
 }
+
+
 
 export default UserPanel;
