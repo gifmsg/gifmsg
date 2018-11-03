54c918ad9524a32d9cd20d44407a53cebd584b2a
Paul Suarez <paulsblogging.com@gmail.com>
2018-11-02 23:49:09 -0700

    register ui is built

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var config = {
+    apiKey: "AIzaSyBvR3Qk6Fafe-VtS6dvX7mGAoSn7NRpk7o",
+    authDomain: "gifmsg-b1bc4.firebaseapp.com",
+    databaseURL: "https://gifmsg-b1bc4.firebaseio.com",
+    projectId: "gifmsg-b1bc4",
+    storageBucket: "gifmsg-b1bc4.appspot.comsosdf",
+    messagingSenderId: "205206609070"
+  };
+firebase.initializeApp(config);
+  
+export default firebase;
